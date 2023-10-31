55a55aab4b2ccb1c74245aab755c4378696a0d48
Gleidson Medrado <gleidsonmedrado@hotmail.com>
2023-10-30 21:08:28 -0300

    Programando autorização de localização

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { View, Text, StatusBar, StyleSheet } from 'react-native';
 import MapView from "react-native-maps"
 import * as Location from "expo-location"
 
 export default function App() {
+
+  const [ localizacao, definirLocalizacao ] = useState({})
+
+  useEffect(function() {
+    async function ObterLocalizacao() {
+      await Location.requestForegroundPermissionsAsync()
+      definirLocalizacao(await Location.getCurrentPositionAsync( {} ))
+    }
+
+    ObterLocalizacao()
+  }, [])
+  
   return <View>
     <StatusBar barStyle="light-content" backgroundColor="#144272"/>
   </View>
